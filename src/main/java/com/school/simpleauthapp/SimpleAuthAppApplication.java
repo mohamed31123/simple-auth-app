f58912fcd12c4ce4ed996396495fe91d2ec9b883
mohamed <m.eddinari5197@uca.ac.ma>
2026-08-08 23:08:27 +0200

    first commit

---
+++ src/main/java/com/school/simpleauthapp/SimpleAuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.school.simpleauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAuthAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/school/simpleauthapp/SimpleAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.simpleauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuthAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
